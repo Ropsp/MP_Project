14a93bc3224f2c8506b1d69069bfa597b1b33be5
Roope Stenvall <roopestenvall@gmail.com>
2020-05-18 08:15:11 +0300

    test commit

---
+++ camraProject/src/gallery.component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Image, ScrollView } from 'react-native';
+
+import styles from './styles';
+// Luokka, joka näyttää otetut kuvat horisontaaliseti nappuloiden päällä.
+// Voi liikuttaa horisontaalisesti.
+export default ({captures=[]}) => (
+    <ScrollView 
+        horizontal={true}
+        style={[styles.bottomToolbar, styles.galleryContainer]} 
+    >
+        {captures.map(({ uri }) => (
+            <View style={styles.galleryImageContainer} key={uri}>
+                <Image source={{ uri }} style={styles.galleryImage} />
+            </View>
+        ))}
+    </ScrollView>
+);
